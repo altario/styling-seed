c81052bf1163afc0d6ed65385590e1f136ca295e
Joao Ribeiro <jonnybgod@gmail.com>
2018-03-16 14:52:14 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+// angular
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// app
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
